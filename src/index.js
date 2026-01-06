eff161c039732fe96df8285b9cc95f37977fbde0
Kho23 <wndrn2002@naver.com>
2026-01-06 13:25:36 +0900

    리액트 환경변수 인식 못해서 변ㄱ여

--- src/index.js
+++ src/index.js
@@ -9,11 +9,9 @@ import { getCookie } from "./util/cookieUtil";
 import axios from "axios";
 import { logout } from "./store/auth/authSlice";
 
-// [수정] 현재 실제 사용 중인 AWS 서버 IP인 13.238.88.253으로 변경합니다.
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL || "http://13.238.88.253:8080";
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || "https://api.jeocenter.shop";
 axios.defaults.headers.common["Content-Type"] = "application/json";
-// [추가] CORS 환경에서 쿠키와 인증 헤더를 주고받기 위해 필수
-axios.defaults.withCredentials = true; 
+axios.defaults.withCredentials = true;
 
 axios.interceptors.request.use(
   (config) => {
